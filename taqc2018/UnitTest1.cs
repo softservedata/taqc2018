fb0ba00906951abee6c5d82ab50d76962dffc837
garasym <garasym@gmail.com>
2018-08-01 10:17:53 +0300

    Add project files.

---
+++ taqc2018/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace taqc2018
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
